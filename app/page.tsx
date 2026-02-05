657588c4980589b421f5bb61f731a81dde377da4
Andre Paxe <baratexangola@gmail.com>
2026-02-05 14:53:03 +0100

    Fix: estrutura de renderização da Home e CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-// app/page.tsx
-import Hero from '../components/Hero';
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import WhatsAppWidget from "../components/WhatsAppWidget";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen bg-white">
       <Navbar />
       <Hero />
+      <About />
+      <Contact />
+      <WhatsAppWidget />
     </main>
   );
 }
